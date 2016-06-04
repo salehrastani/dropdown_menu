9f981ca95a93b5a9fd669ba154a71d3cd4c2ca68
Saleh Rastani <salehrastani@gmail.com>
2016-06-03 17:51:15 -0700

    Add campground drop-down with nested dropdowns using javascript.

---
+++ dropdown_menu.js
@@ -0,0 +1,61 @@
+var listOfCampgrounds = [
+  {
+    'title': 'Tahoe',
+    'submenu': null
+  },
+  {
+    'title': 'Death Valley',
+    'submenu': null
+  },
+  {
+    'title': 'Yosemite',
+    'submenu': [
+      {
+        'title': 'Curry Village',
+        'submenu': null
+      },
+      {
+        'title': 'Redbud',
+        'submenu': null
+      },
+      {
+        'title': 'North Pines',
+        'submenu': [
+          {
+            'title': '101',
+            'submenu': null
+          },
+          {
+            'title': '137',
+            'submenu': null
+          },
+          {
+            'title': '290',
+            'submenu': null
+          }
+        ]
+      }
+    ]
+  }
+];
+
+
+
+var source   = $("#dropdown-menu").html();
+var template = Handlebars.compile(source);
+
+$('body').append(template(listOfCampgrounds));
+
+$('.menu-item').click(function(event){
+  event.stopPropagation();
+  alert(event.currentTarget.firstElementChild.innerHTML)
+});
+
+
+$( ".menu-item" ).hover(
+  function() {
+    $(this).children("ul:first").css('display', 'inline-block')
+  }, function(){
+    $(this).children("ul:first").css('display', 'none')
+  }
+);
